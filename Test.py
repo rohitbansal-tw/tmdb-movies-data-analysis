191a5cbd27f39d543739b69350f6e9e038ef2e23
rohitbansal-tw <rohitbansal-tw>
2024-04-24 03:39:52 +0000

    Initial Commit

--- Test.py
+++ Test.py
@@ -1,4 +1,5 @@
 # Databricks notebook source
 # MAGIC %pip uninstall -y databricks_helpers exercise_ev_databricks_unit_tests
 # MAGIC %pip install git+https://github.com/data-derp/databricks_helpers#egg=databricks_helpers
+# MAGIC
 # MAGIC %pip install pandas
